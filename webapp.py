c8c810ce8f75c0bf8b240304ed642e135905903a
JeremyKnight <jeremyknightjck99@gmail.com>
2018-01-29 09:36:10 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -15,6 +15,26 @@ app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.
 @app.route('/')
 def renderMain():
     return render_template('home.html')
+  
+@app.route('q1')
+def renderMain():
+    return render_template('question1.html')
+  
+@app.route('q2')
+def renderMain():
+    return render_template('question2.html')
+
+@app.route('q3')
+def renderMain():
+    return render_template('question3.html')
+  
+@app.route('q4')
+def renderMain():
+    return render_template('question4.html')
+
+@app.route('q5')
+def renderMain():
+    return render_template('question5.html')
 
 if __name__=="__main__":
     app.run(debug=True)
