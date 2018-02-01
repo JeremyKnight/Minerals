9df373e2e3ebe22756f8ec9e0bfe9b49697e6dbb
Preston <P-Nasty>
2018-02-01 11:04:42 -0800

    dsfdsa

--- webapp.py
+++ webapp.py
@@ -15,26 +15,37 @@ app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.
 @app.route('/')
 def renderMain():
     return render_template('home.html')
-  
+   
+   
+
+@app.route('/startOver')
+def startOver():
+    session.clear()
+    return redirect(url_for('renderMain'))
 @app.route('/q1')
 def renderQuestion1():
-    return render_template('question1.html')
-  
+    return render_template('question1.html',methods=['GET','POST'])
+    
 @app.route('/q2')
 def renderQuestion2():
-    return render_template('question2.html')
+    return render_template('question2.html',methods=['GET','POST'])
 
 @app.route('/q3')
 def renderQuestion3():
-    return render_template('question3.html')
+    return render_template('question3.html',methods=['GET','POST'])
   
 @app.route('/q4')
 def renderQuestion4():
-    return render_template('question4.html')
+    return render_template('question4.html',methods=['GET','POST'])
 
 @app.route('/q5')
 def renderQuestion5():
-    return render_template('question5.html')
+    return render_template('question5.html',methods=['GET','POST'])
 
+@app.route('/f1',methods=['GET','POST'])
+def renderAnswer():
+    return render_template('final.html',methods=['GET','POST'])
+
+    
 if __name__=="__main__":
     app.run(debug=True)
