2c1dfcc0ab470b32d79846233bfe9cc198bbef3b
Preston <P-Nasty>
2018-02-07 10:49:04 -0800

    spicy

--- webapp.py
+++ webapp.py
@@ -10,12 +10,12 @@ app = Flask(__name__)
 # See: http://flask.pocoo.org/docs/0.10/quickstart/#sessions
 
 app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.
- session['correct'] = 0                                    #The value should be set in Heroku (Settings->Config Vars).
+                                    #The value should be set in Heroku (Settings->Config Vars).
 
 @app.route('/')
 def renderMain():
     return render_template('home.html')
-   
+    session['correct'] = 0
    
 
 @app.route('/startOver')
@@ -25,37 +25,33 @@ def startOver():
 @app.route('/q1',methods=['GET','POST'])
 def renderQuestion1():
     return render_template('question1.html')
-    
+    if 'Quartz' == request.form["a"]:
+        session['correct'] += 1 
 @app.route('/q2',methods=['GET','POST'])
 def renderQuestion2():
     return render_template('question2.html')
-
+    if 'Color' == request.form["b"]:
+        session['correct'] += 1  
 @app.route('/q3',methods=['GET','POST'])
 def renderQuestion3():
     return render_template('question3.html')
-  
+    if 'Halite' == request.form["c"]:
+        session['correct'] += 1 
 @app.route('/q4',methods=['GET','POST'])
 def renderQuestion4():
     return render_template('question4.html')
+    if 'Apatite' == request.form["d"]:
+        session['correct'] += 1 
 @app.route('/q5',methods=['GET','POST'])
 def renderQuestion5():
     return render_template('question5.html')
-
+    if '5' == request.form["e"]:
+        session['correct'] += 1 
 @app.route('/f1',methods=['GET','POST'])
 def renderAnswer():
-    session['correct'] = 0
-    if 'Quartz' == request.form["a"]:
-        session['correct'] += 1 
-    if 'Color' == request.form["b"]:
-        session['correct'] += 1 
-    if 'Halite' == request.form["c"]:
-        session['correct'] += 1 
-    if 'Apatite' == request.form["d"]:
-        session['correct'] += 1 
-    if '5' == request.form["e"]:
-        session['correct'] += 1 """
-    return render_template('final.html')
-
+    print(session)
+    return render_template('final.html', correct = session['correct'] )
+    
     
 if __name__=="__main__":
     app.run(debug=True)
